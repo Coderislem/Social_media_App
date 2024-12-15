aa12d851576c7691b1be4d5a524c36d88f18ec5e
Coderislem <almohtrf2003@gmail.com>
2024-12-15 21:02:40 +0100

    friend managment

--- friends/urls.py
+++ friends/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import profiles_view,add_friend,FreindRequestSuggestions
+from .views import profiles_view,add_friend,FreindRequestSuggestions,Friends,remove_friend
 urlpatterns = [
     path('profiles/',FreindRequestSuggestions,name="profiles"),
-    path('add-friend/<int:reciver_id>/',add_friend,name="add-friend")
+    path('add-friend/<int:reciver_id>/',add_friend,name="add-friend"),
+    path('friends',Friends,name="friends"),
+    path('remove/<int:id>/',remove_friend,name="remove")
 ]+static(settings.MEDIA_URL,document_root = settings.MEDIA_URL)
--- friends/views.py
+++ friends/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render,redirect
-from .models import FriendRequest
+from .models import FriendRequest,Friendship
 from accounts.models import Profile
 from django.contrib.auth.models import User
 from django.shortcuts import get_object_or_404
 from django.contrib import messages
+from django.db.models import Q
 # Create your views here.
 
 def profiles_view(request):
@@ -38,6 +39,25 @@ def FreindRequestSuggestions(request):
     all_exculded_ids = list(send_requests) + list(receiver_requests)
     profile_suggestions = Profile.objects.exclude(id__in = all_exculded_ids).exclude(id = current_profile.id)
     return render(request,'profiles.html',{"profile_suggestions":profile_suggestions})
-# def Friends(request):
+def Friends(request):
+    profile = request.user.profile
 
-#     friends = Friendship.objects.filter
+    friends_1 = Friendship.objects.filter(profile1 = profile).values_list("profile2",flat=True)
+    freinds_2 = Friendship.objects.filter(profile2 = profile).values_list("profile1",flat=True)
+    friends_ids = list(friends_1) + list(freinds_2)
+    friends_profile = Profile.objects.filter(id__in =friends_ids)
+    return render(request,"friends.html",{"friends_profile":friends_profile})
+
+def get_friend(request,id):
+    friend = get_object_or_404(Profile,pk=id)
+    current_profile = request.user.profile
+    try:
+        friend_ship = Friendship.objects.filter( Q(profile1 = current_profile , profile2 = friend) | Q(profile2 = current_profile,profile1 = friend))
+        return friend_ship
+    except Friendship.DoesNotExist:
+        return None
+def remove_friend(request,id):
+    friend_ship = get_friend(request,id)
+    friend_ship.delete()
+    messages.success(request,"friend delete successfully !")
+    return redirect(request.META.get('HTTP_REFERER', '/') ) 
